5288e9f11ac24f1feca1ca07cc99150c7b9403f0
oseias-romeiro <oseiasromeirom@gmail.com>
2023-08-14 17:43:33 -0300

    docker

--- seeders/20230811015244-create-initial-users.js
+++ seeders/20230811015244-create-initial-users.js
@@ -1,20 +1,40 @@
 'use strict';
-
 const bcrypt = require('bcrypt');
-const { User } = require('../models');
+
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Users', [{
-      ID: 1,
-      name: "jhon",
-      email: "jhon@email.com",
-      role: "ADMIN",
-      password: await bcrypt.hash('jhon', 10),
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }], {});
+    const users = [
+      {
+        id: 1,
+        name: "admin",
+        email: "admin@email.com",
+        role: "ADMIN",
+        password: await bcrypt.hash('admin', 10),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 2,
+        name: "jhon",
+        email: "jhon@email.com",
+        role: "USER",
+        password: await bcrypt.hash('jhon', 10),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 3,
+        name: "marie",
+        email: "marie@email.com",
+        role: "USER",
+        password: await bcrypt.hash('marie', 10),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]
+    await queryInterface.bulkInsert('Users', users, {});
   },
 
   async down (queryInterface, Sequelize) {
